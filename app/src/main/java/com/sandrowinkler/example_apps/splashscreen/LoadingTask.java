e1bf3e6af85da6b84e13668b276f5de0e1381190
Sandro Winkler <code@sandrowinkler.com>
2016-12-26 15:30:47 +0100

    Add logging to loading task

--- app/src/main/java/com/sandrowinkler/example_apps/splashscreen/LoadingTask.java
+++ app/src/main/java/com/sandrowinkler/example_apps/splashscreen/LoadingTask.java
@@ -1,6 +1,7 @@
 package com.sandrowinkler.example_apps.splashscreen;
 
 import android.os.AsyncTask;
+import android.util.Log;
 import android.widget.ProgressBar;
 
 /**
@@ -27,6 +28,7 @@ public class LoadingTask extends AsyncTask<Void, Integer, Void> {
 
     @Override
     protected Void doInBackground(Void... params) {
+        log("Begin loading: numberOfSteps = " + numberOfSteps + ", secondsBetweenSteps = " + secondsBetweenSteps);
         load();
         return null;
     }
@@ -35,6 +37,8 @@ public class LoadingTask extends AsyncTask<Void, Integer, Void> {
         for (int currentStep = 0; currentStep < numberOfSteps && !isCancelled(); currentStep++) {
             final int progressInPercent = 100 * currentStep / numberOfSteps;
             publishProgress(progressInPercent);
+
+            log(progressInPercent + " %");
 
             try {
                 Thread.sleep((long)(secondsBetweenSteps * 1000));
@@ -50,6 +54,16 @@ public class LoadingTask extends AsyncTask<Void, Integer, Void> {
 
     @Override
     protected void onPostExecute(Void aVoid) {
+        log("Loading finished");
         listener.onLoadingFinished();
+    }
+
+    @Override
+    protected void onCancelled(Void aVoid) {
+        log("Loading cancelled");
+    }
+
+    private void log(final String message) {
+        Log.i(LoadingTask.class.getSimpleName(), message);
     }
 }
